76bca761343d5f093adf24f36e7eb945a864eb0a
JennyMThornton <JennyMThornton>
2017-01-21 14:17:21 +0000

    Emmision Colour Changes

--- Assets/Codebase/WaveTarget.cs
+++ Assets/Codebase/WaveTarget.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class WaveTarget : MonoBehaviour {
 
+    Color m_currentCol = Color.white;
+    public Material[] m_mats = null;
+
 	void Start () {
 	
 	}
@@ -16,10 +19,32 @@ public class WaveTarget : MonoBehaviour {
 	}
 
 	public virtual void WaveUpdate(WaveGun wavegun) {
+        UpdateMatCols(wavegun.GetWaveColor());
 
-	}
+    }
 
 	public virtual void WaveEnd(WaveGun wavegun) {
 
 	}
+    void UpdateMatCols(Color newCol)
+    {
+
+        if (m_mats != null)
+        {
+            if (m_currentCol == newCol)
+            {
+                return;
+            }
+
+            m_currentCol = newCol;
+            for (int i = 0; i < m_mats.Length; i++)
+            {
+                Material currentMat = m_mats[i];
+                if (currentMat != null)
+                {
+                    currentMat.SetColor("_Emission", newCol);
+                }
+            }
+        }
+    }
 }
